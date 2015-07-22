5b8f9cebcb287d4ff0bd8d704b7a0683bfd61b0a
adhayanapaul <adhayana.paul@gmail.com>
2015-07-22 01:35:07 -0700

    Condense
    Condensed the code

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ var itemList = [
 					{"item": "person"},
 					{"item": "flower"} //first object
 				];
+
+
+/*var songslist = [
+	{"song":"Satellite","lyrics":"You can't feel the heat until you hold your hand over the flame You have to cross the line just to remember where it lays"},
+	{"song":"Paper Wings","lyrics":"One last thing I beg you please just before you go"},
+	{"song":"Ready to Fall","lyrics":"Hold on slow down again from the top now and tell me everything"}
+	];
+window.onload = function(){
+	console.log(JSON.stringify(contactlist));
+	for(var i=0;i < contactlist.length;i++){
+		var songs=songslist[i];
+		displayContact(songs);
+	}
+	
+};
+var displayContact = function(songs){
+	var contactString = "Name: " + songs.song + " Number: " + songs.lyrics;
+	var htmlString = '<div data-role="collapsible">\
+						<h3> '+ songs.song +' </h3>\
+						<p>'+ songs.lyrics +'</p>\
+					</div>';
+	$('#songHolder').append(htmlString);
+	$("#songHolder").collapsibleset("refresh");
+	console.log(contactString);
+};*/
